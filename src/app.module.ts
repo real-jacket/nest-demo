7538bcc0bf1b29d5baf2e63adf89c2cbedd0ab90
real-jacket <1762982273@qq.com>
2023-06-05 21:36:13 +0800

    feat: middleware log

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { logger } from './middlewares/logger.middleware';
+// import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        // LoggerMiddleware
+        logger,
+      )
+      .forRoutes({
+        path: 'cats',
+        method: RequestMethod.ALL,
+      });
+  }
+}
